b04ff8a1dd7a2684ea126e200ec0a659597329ce
wx-chevalier <384924552@qq.com>
2022-06-08 09:10:43 +0800

    feat: update articles or snippets

---
+++ server.js
@@ -0,0 +1,20 @@
+var Renderer = require("docsify-server-renderer");
+var readFileSync = require("fs").readFileSync;
+
+// init
+var renderer = new Renderer({
+  template: readFileSync("./index.template.html", "utf-8"),
+  config: {
+    name: "docsify",
+    repo: "docsifyjs/docsify",
+  },
+});
+
+renderer
+  .renderToString("https://ng-tech.icu/")
+  .then((html) => {
+    console.log(html);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
